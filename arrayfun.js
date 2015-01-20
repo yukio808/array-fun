ae7cf33bf6513127f9b22ba6c5ef5537aeb69a88
yukio808 <aj.yamamoto@me.com>
2015-01-19 16:37:24 -1000

    added functiona 1/2 way done

--- arrayfun.js
+++ arrayfun.js
@@ -6,10 +6,28 @@ module.exports = {
       result1 = array.slice(0, n);
       return result1;
     }
+  },
+
+    max : function (array) {
+     //var arraym = .map();
+      var arraymax = Math.max.apply(null, array);
+      return arraymax;
+    
+  },
+
+    min : function (array) {
+     // var arraymin = .map();
+      var arraymin = Math.min.apply(null, array);
+      console.log(arraymin);
+      return arraymin;
+
+  },
+    cont : function (array, target){
+      for(i = 0; i < array.length; i++){
+        if(array[i]=== target) {
+          return true; 
+        }
+      }
+      return false;
   }
 };
- // module.exports = {
- //   min : function () {
-
-    // body...
-//}
